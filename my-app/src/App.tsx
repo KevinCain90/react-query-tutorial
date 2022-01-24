50a984708b66196a65eca46138a4cb32a7018445
Kevin Cain <kevincain@Kevins-MacBook-Pro.local>
2022-01-24 15:18:12 +0000

    reverting back and starting again

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
-
-const TEST = styled.p`
-  background-color: green;
-`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TEST>
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </TEST>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
