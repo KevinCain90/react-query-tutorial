675d65a76ddb7409842324ee6ebc2a6a5a045b70
Kevin Cain <kevincain@Kevins-MacBook-Pro.local>
2022-01-25 08:56:28 +0000

    added data mocks based on info on screenshots provided and typed them

---
+++ my-app/src/components/BookingContainer/dataMocks.ts
@@ -0,0 +1,50 @@
+import { Destinations } from './types';
+
+export const destinations: Destinations = [
+  {
+    hotelName: "Iberostar Grand Salome",
+    description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.",
+    price: 1136.50,
+    departureAirport: "East Midlands",
+    location: "Costa Adeje, Tenerife",
+    hotelImage: "hotel-image-2.png",
+    starRating: 5,
+    guests: {
+      adults: 2,
+      children: 2,
+      infant: 1
+    },
+    departureDate: "3rd July 2021",
+    holidayLength: "7 days",
+  },
+  {
+    hotelName: "Aguamarina Golf Hotel",
+    description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.",
+    price: 696.80,
+    departureAirport: "Liverpool",
+    location: "Costa Adeje, Tenerife",
+    hotelImage: "hotel-image-1.png",
+    starRating: 4,
+    guests: {
+      adults: 2,
+      children: 1,
+    },
+    departureDate: "27th May 2021",
+    holidayLength: "7 days",
+  },
+  {
+    hotelName: "Las Piramides Resort",
+    description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.",
+    price: 499.99,
+    departureAirport: "Manchester",
+    location: "Costa Adeje, Tenerife",
+    hotelImage: "hotel-image-1.png",
+    starRating: 3,
+    guests: {
+      adults: 2,
+      children: 2,
+    },
+    departureDate: "27th May 2021",
+    holidayLength: "7 days",
+  },
+];
