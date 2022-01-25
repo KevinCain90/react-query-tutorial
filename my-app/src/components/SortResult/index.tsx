a5d4d76d5bdc5ac4c8c9aaf530d16b9519f86aa7
Kevin Cain <kevincain@Kevins-MacBook-Pro.local>
2022-01-25 11:34:46 +0000

    first version of sort result component that displays options

--- my-app/src/components/SortResult/index.tsx
+++ my-app/src/components/SortResult/index.tsx
@@ -1,14 +1,28 @@
 import { SortResultProps } from "./types";
 
+import { faSortAlphaDown, faPoundSign, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const SortResult: React.FC<SortResultProps> = ({
   currentSelection,
   options,
   sortBy,
 }) => {
-  console.log({currentSelection}, {options}, sortBy)
   return (
     <div data-testid="sort-result">
-      Sort Result
+      {options.map(option => {
+        return (
+          <div key={option.name} onClick={() => sortBy(option.name)}>
+            <span>
+              sort 
+              {option.name !== 'Hotel' ? ' by ' : ' '}
+              <strong>{option.name}</strong>
+            </span>
+            {' '}
+            <FontAwesomeIcon icon={option.icon === 'alpha' ? faSortAlphaDown : option.icon === 'starRating' ? faStar : faPoundSign } />
+          </div>
+        )
+      })}
     </div>
   )
 };
