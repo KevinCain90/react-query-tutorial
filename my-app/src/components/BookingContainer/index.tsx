1d7ffbe0e597f19e7e1cbd24de22079c156c2485
Kevin Cain <kevincain@Kevins-MacBook-Pro.local>
2022-01-25 09:25:57 +0000

    added config to be able to use css modules in create react app

---
+++ my-app/src/components/BookingContainer/index.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+
+import { ResultCard } from '../ResultCard';
+import { SortResult } from '../SortResult';
+
+import { destinations } from './dataMocks';
+import styles from './index.modules.css';
